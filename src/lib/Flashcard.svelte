d0f9a5b40a09be4e68f575e8103139ccc1f62e06
Gav Hern <contact@gavhern.com>
2021-11-23 16:49:19 -0800

    HIde animating number on flashcard page from screenreaders

--- src/lib/Flashcard.svelte
+++ src/lib/Flashcard.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	export let ariaHidden: string = "false";
+	export let ariaHidden: "true" | "false" = "false";
 	export let style: string = "";
 </script>
 
 <div style={style} class="absolute grid place-items-center w-full h-full">
 	<div class="bg-gray-100 dark:bg-gray-800 shadow-lg dark:shadow-none w-4/5 h-4/5 grid place-items-center rounded-2xl">
-		<h1 class="text-7xl xl:text-8xl font-semibold tracking-widest text-indigo-600 dark:text-indigo-400"><slot></slot></h1>
+		<strong class="text-7xl xl:text-8xl font-semibold tracking-widest text-indigo-600 dark:text-indigo-400" aria-hidden={ariaHidden}><slot></slot></strong>
 	</div>
 </div>
