85f2f56d48dc221e6c1d2766ac3008518aba2550
Gav Hern <contact@gavhern.com>
2022-01-26 22:10:43 -0800

    Add neighbor page

---
+++ src/routes/neighbor.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
+<svelte:head>
+	<title>Stack Tutor - Neighbor</title>
+</svelte:head>
+
+<section in:fade={{ duration: 300, delay: 300 }} out:fade={{ duration:300 }} class="text-center">
+
+	<h1 class="pt-16 text-4xl font-semibold tracking-wider mb-4 dark:text-gray-200">Coming soon!</h1>
+
+	<a href="/" class="text-indigo-600 dark:text-indigo-400 underline">Back to home</a>
+
+</section>
