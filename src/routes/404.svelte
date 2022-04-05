b16527ab6127d925308ca8c11fdedfdc09badb33
Gav Hern <contact@gavhern.com>
2022-04-04 21:43:12 -0700

    Add 404 page with netlify rewrite

---
+++ src/routes/404.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
+<svelte:head>
+	<title>Stack Tutor - Not Found</title>
+</svelte:head>
+
+<section in:fade={{ duration: 300, delay: 300 }} out:fade={{ duration:300 }} class="text-center">
+
+	<h1 class="pt-16 text-4xl font-semibold tracking-wider mb-4 dark:text-gray-200">404 Not Found</h1>
+
+	<a href="/" class="text-indigo-600 dark:text-indigo-400 underline">Back to home</a>
+
+</section>
